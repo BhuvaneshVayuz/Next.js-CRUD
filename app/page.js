1f9cc3205b07d7f9f0d30de7c1acfaeec7d13679
Rahul mahto <rahulm.vayuz@gmail.com>
2024-12-06 12:03:00 +0530

    merge

--- app/page.js
+++ app/page.js
@@ -7,56 +7,6 @@ import Head from "next/head";
 export default function Home() {
 
 
-  function addProductJsonLd() {
-    return {
-      __html: `{
-      "@context": "https://schema.org/",
-      "@type": "Product",
-      "name": "Executive Anvil",
-      "image": [
-        "https://example.com/photos/1x1/photo.jpg",
-        "https://example.com/photos/4x3/photo.jpg",
-        "https://example.com/photos/16x9/photo.jpg"
-       ],
-      "description": "Sleeker than ACME's Classic Anvil, the Executive Anvil is perfect for the business traveler looking for something to drop from a height.",
-      "sku": "0446310786",
-      "mpn": "925872",
-      "brand": {
-        "@type": "Brand",
-        "name": "ACME"
-      },
-      "review": {
-        "@type": "Review",
-        "reviewRating": {
-          "@type": "Rating",
-          "ratingValue": "4",
-          "bestRating": "5"
-        },
-        "author": {
-          "@type": "Person",
-          "name": "Fred Benson"
-        }
-      },
-      "aggregateRating": {
-        "@type": "AggregateRating",
-        "ratingValue": "4.4",
-        "reviewCount": "89"
-      },
-      "offers": {
-        "@type": "Offer",
-        "url": "https://example.com/anvil",
-        "priceCurrency": "USD",
-        "price": "119.99",
-        "priceValidUntil": "2020-11-20",
-        "itemCondition": "https://schema.org/UsedCondition",
-        "availability": "https://schema.org/InStock"
-      }
-    }
-  `,
-    };
-  }
-
-
 
   // Handlers for button navigation
   return (
@@ -68,11 +18,21 @@ export default function Home() {
           content="Super product with free shipping."
           key="desc"
         />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={addProductJsonLd()}
-          key="product-jsonld"
-        />
+       <script type="application/ld+json">
+    {`${{
+      "@context": "https://schema.org/",
+      "@type": "ImageObject",
+      "contentUrl": "https://example.com/photos/1x1/black-labrador-puppy.jpg",
+      "license": "https://example.com/license",
+      "acquireLicensePage": "https://example.com/how-to-use-my-images",
+      "creditText": "Labrador PhotoLab",
+      "creator": {
+        "@type": "Person",
+        "name": "Brixton Brownstone"
+       },
+      "copyrightNotice": "Clara Kent"
+    }}`}
+    </script>
       </Head>
     
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-8 sm:p-20 font-sans">
